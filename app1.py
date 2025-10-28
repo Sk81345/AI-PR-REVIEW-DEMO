7eb8f02154e61f8bb3994c4fd99211963f8835a9
shyam <user.name>
2025-10-28 12:33:57 +0530

    only added one file

--- app1.py
+++
@@ -1,4 +0,0 @@
-def check_list(data):
-    # Bug: This comparison is backwards, always returning True if the list has 1 or more elements.
-    if 0 < len(data):
-        return data[len(data)] # Bug: This will cause an IndexError (Index out of range)
