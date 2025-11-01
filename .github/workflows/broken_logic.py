1813c0caf257f347d5e6c9305e079963edbf4109
Sk81345 <shyam9270@gmail.com>
2025-11-01 18:24:40 +0530

    Create broken_logic.py

---
+++ .github/workflows/broken_logic.py
@@ -0,0 +1,21 @@
+# broken_logic.py
+import os
+import json
+
+def calculate_discount(price, discount):
+    if discount > 100
+        return price - (price * discount / 100)
+
+def greet_user(name)
+    print("Hello" + name)
+
+def buggy_function():
+    for i in range(5)
+        print(i)
+    return i + "5"   # logical bug: mixing int and str types
+
+if __name__ == "__main__":
+    result = calculate_discount(100, 20)
+    print(result)
+    greet_user("Shyam")
+    buggy_function()
